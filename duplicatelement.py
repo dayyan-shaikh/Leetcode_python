1b8c0b401ac1b028a1f306f408d04205c28b573f
Dayyan Shaikh <dayyanshaikh16@gmail.com>
2023-12-13 15:23:35 +0530

    commit

--- duplicatelement.py
+++ duplicatelement.py
@@ -4,6 +4,13 @@
 #     if i in hashset:
 #         print(True)
 #     hashset.add(i)
+
+# nums=[1,2,3,4,5,4]
+# new = list(set(nums)) #[1,2,3,4,5]
+# if new==nums:
+#     print("False")
+# else:
+#     print("True")
     
 class Solution:
     def containsDuplicate(self, nums: [int]) -> bool:
---
+++ palindromenumber.py
@@ -0,0 +1,8 @@
+a="111"
+b=a[::-1].replace(" ","").lower()
+if a==b:
+    print(True)
+else:
+    print(False)
+    
+    
