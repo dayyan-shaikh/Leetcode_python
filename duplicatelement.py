ce86daea0a5643189be3e5f389c7825d84068539
Dayyan Shaikh <dayyanshaikh16@gmail.com>
2023-12-14 13:35:49 +0530

    commit

--- duplicatelement.py
+++ duplicatelement.py
@@ -1,7 +1,7 @@
-# nums=[1,2,3,4,5,4]
-# new = list(set(nums)) #[1,2,3,4,5]
-# if new==nums:
-#     print("False")
-# else:
-#     print("True")
+nums=[1,2,3,4,5,4]
+new = list(set(nums)) #[1,2,3,4,5]
+if new==nums:
+    print("False")
+else:
+    print("True")
     
