acd30089a024c016c488e1333ff4bf9e45b4a137
Dayyan Shaikh <dayyanshaikh16@gmail.com>
2023-12-15 00:33:40 +0530

    commit

--- new.py
+++ new.py
@@ -1,15 +1,11 @@
-# nums=[0,0,1,1,1,2,2,3,4,4,3]
-# l=list(set(nums))
-# print(l)
-
-a="A man, a plan, a canal: Panama"
-b=a[::-1].replace(" ","").replace(":","").replace(",","").lower()
-c=a.replace(" ","").replace(":","").replace(",","").lower()
-print(b)
-print(c)
-if b==c:
+s="A man, a plan, a canal: Panama"
+r=''
+for i in s:
+    if(i.isalnum()):
+        r += i
+r=r.lower()
+print(r)
+if(r==r[::-1]):
     print(True)
 else:
     print(False)
-    
-    
