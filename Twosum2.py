a6418bed8fb83b0cfdc9b9d99edf8c655461d086
Dayyan Shaikh <dayyanshaikh16@gmail.com>
2023-12-16 11:31:33 +0530

    commit

--- Twosum2.py
+++ Twosum2.py
@@ -1,22 +1,3 @@
-# # nums = [1,2,3,1,2,3]
-# # k = 2
-# nums=[1]
-# k=1
-# l=0
-
-# for i in range(len(nums)):
-#     for j in range(i+1,len(nums)):
-#         if (len(nums)==1):
-#             print(k)
-#         else:
-#             nums[i]==nums[j]
-#             l=i-j
-# print(l)
-# if l<=k:
-#     print(True)
-# else:
-#     print(False)
-
 numbers = [2,7,11,15]
 target = 9
 
---
+++ new.py
@@ -0,0 +1,18 @@
+# # nums = [1,2,3,1,2,3]
+# # k = 2
+# nums=[1]
+# k=1
+# l=0
+
+# for i in range(len(nums)):
+#     for j in range(i+1,len(nums)):
+#         if (len(nums)==1):
+#             print(k)
+#         else:
+#             nums[i]==nums[j]
+#             l=i-j
+# print(l)
+# if l<=k:
+#     print(True)
+# else:
+#     print(False)
