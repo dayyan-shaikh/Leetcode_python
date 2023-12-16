daf8919989a549ee83f409bc95a2bf5e2937016a
Dayyan Shaikh <dayyanshaikh16@gmail.com>
2023-12-16 11:30:41 +0530

    commit

--- Twosum2.py
+++ Twosum2.py
@@ -17,3 +17,18 @@
 # else:
 #     print(False)
 
+numbers = [2,7,11,15]
+target = 9
+
+left=0
+right=len(numbers)-1
+while(left<right):
+    totalsum=numbers[left]+numbers[right]
+    if(totalsum>target):
+        right -= 1
+    elif(totalsum<target):
+        left += 1
+    else:
+        print([left+1,right+1]) 
+        break
+        
