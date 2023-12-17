2a0f216dad35159e1b2062642836dd2e770c13cc
Dayyan Shaikh <dayyanshaikh16@gmail.com>
2023-12-17 23:24:38 +0530

    commit

--- maxprdtof2ele.py
+++ maxprdtof2ele.py
@@ -1,8 +1,17 @@
-# nums=[3,4,5,2]
-nums=[1,5,4,5]
+nums=[3,4,5,2]
+# method1
+# nums=[1,5,4,5]
 
-frstmax=max(nums)
-nums.remove(frstmax)
+# frstmax=max(nums)
+# nums.remove(frstmax)
 
-scndmax=max(nums)
-print((frstmax-1)*(scndmax-1))
+# scndmax=max(nums)
+# print((frstmax-1)*(scndmax-1))
+
+# method2
+nums.sort()
+n=nums[-1]
+m=nums[-2]
+
+l=(n-1)*(m-1)
+print(l)
