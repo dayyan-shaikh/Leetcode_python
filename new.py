0d5c6260c2873ab9bf3a08d44881cfd000bc3a83
Dayyan Shaikh <dayyanshaikh16@gmail.com>
2023-12-17 19:41:35 +0530

    commit

--- new.py
+++ new.py
@@ -1,11 +1,11 @@
 # paths = [["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]
 # paths =[["pYyNGfBYbm","wxAscRuzOl"],["kzwEQHfwce","pYyNGfBYbm"]]
-# paths =[["B","C"],["D","B"],["C","A"]]
+paths =[["B","C"],["D","B"],["C","A"]]
 
 
 
-# flatList = sum(paths, [])
-# print(flatList[-1])
+flatList = sum(paths, [])
+print(flatList[-1])
 # a=list(set(flatList))
 # print(a)
 # print(a[-1])
