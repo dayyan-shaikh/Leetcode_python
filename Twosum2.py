8bd099c4147a2167b844405194d08a7a565f144b
Dayyan Shaikh <dayyanshaikh16@gmail.com>
2023-12-18 22:38:33 +0530

    commit

--- Twosum2.py
+++ Twosum2.py
@@ -1,6 +1,7 @@
 numbers = [2,7,11,15]
 target = 9
 
+# solution 1
 left=0
 right=len(numbers)-1
 while(left<right):
@@ -13,3 +14,8 @@ while(left<right):
         print([left+1,right+1]) 
         break
         
+# solution 2
+# for i in range(len(numbers)):
+#     for j in range(i+1,len(numbers)):
+#         if numbers[i]+numbers[j]==target:
+#             print([i+1,j+1])
--- new.py
+++ new.py
@@ -7,8 +7,29 @@
 #         print(True)
 #     else:
 #         print(False)
+nums1 = [1,3]
+nums2 = [2]
 
+merged = nums1 + nums2
+print(merged)
 
+# Sort the merged array.
 
+merged.sort()
+
+# Calculate the total number of elements in the merged array.
+total = len(merged)
+print(total)
+
+if total % 2 == 1:
+            # If the total number of elements is odd, return the middle element as the median.
+    print(float(merged[total // 2]))
+else:
+            # If the total number of elements is even, calculate the average of the two middle elements as the median.
+    middle1 = merged[total // 2 - 1]
+    print(middle1)
+    middle2 = merged[total // 2]
+    print(middle2)
+    print((float(middle1) + float(middle2)) / 2.0)
     
 
