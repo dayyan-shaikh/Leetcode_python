a22862f57044b1428e79297fee70b1ef7e2df908
Dayyan Shaikh <dayyanshaikh16@gmail.com>
2023-12-22 10:33:45 +0530

    commit

--- new.py
+++ new.py
@@ -8,3 +8,15 @@
 #         else:
 #             print(money)
 
+
+x="abc"
+y="ahjgbdsghc"
+z=0
+
+for i in range(len(x)-1):
+    for j in range(i+1,len(y)):
+        if x[i]==y[j]:
+            z=i+1
+            
+            print(True)
+
