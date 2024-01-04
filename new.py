93c5d634dcf506ab0eeabe321af00ce481a2caf1
Dayyan Shaikh <dayyanshaikh16@gmail.com>
2024-01-04 17:32:56 +0530

    commit

--- new.py
+++ new.py
@@ -20,3 +20,16 @@
 #         print((True))
 # print(False)
 
+n = 3
+a=[]
+for i in range(1,n+1):
+    if i%3==0 and i%5==0:
+        a.append("FizzBuzz")
+    elif i%3==0:
+        a.append("Fizz")
+    elif i%5==0:
+        a.append("Buzz")
+    else:
+        a.append(i)
+print(a)
+
