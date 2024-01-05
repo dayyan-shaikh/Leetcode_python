9174205e780bbbe907235ebc54e10f2a69293c65
Dayyan Shaikh <dayyanshaikh16@gmail.com>
2024-01-05 17:01:41 +0530

    commit

--- new.py
+++ new.py
@@ -21,3 +21,17 @@
 # print(False)
 
 
+nums = [1,2,3,4,5,6,7]
+k = 3
+# nums = [-1,-100,3,99]
+# k = 2
+# res=nums[-k:]
+# res1=nums[:k+1]
+# r=res+res1
+# print(r)
+for i in range(k):
+    a=nums.pop()
+    print(a)
+    nums.insert(0,a)
+    print(nums)
+# print(nums)
